8a7f55d11436cd987ed553616ed8b47ed9ed3b0b
Razz007 <razzbearie67@gmail.com>
2022-08-22 15:22:26 -0400

    Get even answered

--- JSReview/review2.js
+++ JSReview/review2.js
@@ -1,5 +1,14 @@
 // Arrays, Loops and Reps
+
+// Part II JavaScript Reps
+// Easy Going
 for (let i = 0; i <= 20; i++) {
     console.log(i);
 }
 
+// Get Even
+for (let even = 0; even < 200; even++) {
+    if (even % 2 === 0) {
+        console.log(even);
+    }
+}
