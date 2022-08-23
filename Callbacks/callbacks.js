6b805d4a66623fd312957568351cda94e4b84bd4
Razz007 <razzbearie67@gmail.com>
2022-08-23 13:39:39 -0400

    Add Callbacks folder

---
+++ Callbacks/callbacks.js
@@ -0,0 +1,77 @@
+// Call-back methods ----> .map(), .filter(), .forEach()
+// forEach
+
+// const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0];
+// const panagram = ["The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"];
+// array methods take a coll-back function as an argument
+// call-backs are function that will run at a later time
+
+
+
+// // .every()
+// let test = nums.every(theCallback); // returns either true or false
+// // create call-back function
+// function theCallback(element) {
+//     if (element >= 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// let test2 = panagram.every((element) => element.length < 8)
+// console.log(`The result of our every method check is ${test2}`)
+
+// .filter() Method
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 0];
+const panagram = ["The", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog"];
+
+// const filtered = nums.filter(num => num < 4);
+// const filtered2 = panagram.filter(word => word.length % 2 === 0);
+
+// console.log(filtered);
+// console.log(filtered2);
+
+
+// .find() Array Method
+// const find1 = nums.find(num => num % 5 === 0); // finds first number divisible by 3
+// const find2 = panagram.find(element => element.length >= 5);
+// console.log(find1);
+// console.log(find2);
+
+// .findIndex() Method
+// const findInd = nums.findIndex(num => num % 3 === 0);
+// const findInd2 = panagram.findIndex(element => element.length <= 2)
+// console.log(findInd);
+// console.log(findInd2);
+
+// forEach() Method
+// nums.forEach(element => console.log(element * 3)); // returns undefined (forEach does not return)
+// panagram.forEach(element => console.log(element + "!"));
+
+
+// .map() Method
+// const map1 = nums.map( x => x * 100);
+// const map2 = panagram.map(words => words.toUpperCase());
+// console.log(map1);
+// console.log(map2);
+
+
+// .some() Method tests whether at least one element passes the test implemented by the callback function. Returns a boolean value
+// const divBy7 = (element) => element % 7 === 0;
+// const haveLetter = element => element.includes("a","A");
+// console.log(panagram.some(haveLetter));
+// console.log(nums.some(divBy7));
+
+// .reduce() Method
+
+// add all the numbers in the array
+// const initialVals = 0;
+// const sum = nums.reduce((acumulator, nums) => acumulator + nums, initialVals);
+// console.log(sum);
+
+// // concatenate all the words in panagram array
+// const concat = panagram.reduce((acumulator, word) => acumulator + " " + word, "");
+// console.log(concat);
+
+// .sort() Method
--- JSReview/review2.js
+++ JSReview/review2.js
@@ -1,14 +1,90 @@
 // Arrays, Loops and Reps
+// Arrays, Loop and Reps
 
 // Part II JavaScript Reps
 // Easy Going
-for (let i = 0; i <= 20; i++) {
-    console.log(i);
-}
+// for (let i = 0; i <= 20; i++) {
+//     console.log(i);  
+// }
 
-// Get Even
-for (let even = 0; even < 200; even++) {
-    if (even % 2 === 0) {
-        console.log(even);
-    }
+// // Get Even
+// for (let i = 0; i <= 200; i++) {
+//     if (i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
+
+// //  fizzbuzz
+// const fizzbuzz = function(num) {
+//     for (let i = 1; i <= num; i++) {
+//         if (i % 3 === 0 && i % 5 === 0) {
+//         console.log("FizzBuzz");
+//         } else if (i % 3 === 0) {Fi
+//         console.log("Fizz");
+//         } else if (i % 5 === 0) {
+//         console.log("Buzz");
+//         } else {
+//         console.log(i);
+//         }
+//     }
+//     }
+//     fizzbuzz(30);
+
+
+// Wild Wild Life
+// const wolfy = ["Wolfy", "Wolf", 16, "Yukon Territory"];
+// const sharky = ["Sharky", "shark", 20, "Left Coast"];
+// const plantee = ["Plantee", "plant", 5000, "Mordor"];
+// const porgee = ["Porgee", "porg", 186, "Ahch-To"];
+// const dart = ["D'Art", "Doberman dog", 2, "Upside Down"];
+
+// plantee[2]++;
+// wolfy[3] = "Gotham City";
+// dart.push("Hawkins");
+// wolfy[0] = "Gameboy";
+// console.log(wolfy);
+
+// Yell at the Ninja Turtles
+// const turtle = ["Donatello", "Raphael", "Leonardo", "Michaelangelo"];
+// for (let i = 0; i < turtle.length; i++) {
+//     console.log(turtle[i].toUpperCase());
+// }
+
+// Methods revisited
+// const favMovies = ["Jaws", "The Fellowship of the Ring", "Howl's Moving Castle", "Django Unchained", "Titanic"];
+// console.log(favMovies.indexOf("Titanic")); // returns 4 because it is the 4th item in the array
+// favMovies.sort(); // alphabetical sort
+// favMovies.pop(); // removes last item in array and returns it
+// favMovies.reverse(); // reverse the order of the array
+// favMovies.shift(); // removes first element
+// favMovies.unshift("The Matrix");  // add to the beginning of the array
+// favMovies.splice(3, 1, "Avatar"); // add to the middle of the array
+// let favMoviesSliced = favMovies.slice(2, 4); // returns the 3rd and 4th element of the array. Doesn't change the original array.
+// console.log(favMovies.indexOf("Fast and Furious")); // returns -1 because it is not in the array   
+
+// console.log(favMoviesSliced);
+// console.log(favMovies);
+
+// Where is Waldo?
+// const whereIsWaldo = [
+                    //     ["Timmy", "Frank"], 
+                    // "Eggbert",
+                    //     ["Lucinda", "Jacc", "Neff", "Snoop"],
+                    //     ["Petunia", ["Baked Goods", "Waldo"]];
+
+                    //     let removeEggbert = whereIsWaldo.splice([1], 1);
+                    //     whereIsWaldo[2][2] = "No One"; // changes the 3rd element of the 2nd array to "No One"
+                    //     console.log(whereIsWaldo[2][1][1]); // returns "Waldo"
+                    //     console.log(removeEggbert);
+
+// Excited Kitten
+let str = "";
+let randomString = "Why does hte red dot alweay get away";
+for (let i = 1; i <= 20; i++) {
+    if (str.length % 2 === 0) {
+        str += Math.floor(Math.random(randomString) * randomString);
+    } else {
+    str += "Love me, pet me! HSSSSSS!\n";
 }
+}
+console.log(str);
