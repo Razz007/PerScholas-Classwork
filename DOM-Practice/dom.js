b57b6fd70d31c3528b1dc6d3465030dbdd20c587
Razz007 <razzbearie67@gmail.com>
2022-08-24 15:23:45 -0400

    Create DOM Lab

---
+++ DOM-Practice/dom.js
@@ -0,0 +1,20 @@
+// p tag
+
+// let pEl = document.querySelector(".cool");
+// console.log(pEl);
+// pEl.style.color = "#fff";
+
+
+// // h1 tag
+
+// let h1El = document.getElementById("title");
+// h1El.style.textAlign = "center";
+
+// // Attributes
+// let aEl = document.querySelector("a");
+// aEl.setAttribute("href", "https://www.google.com");
+let liTags3 = document.querySelectorAll(".comment");
+
+for(let li of liTags3) {
+    li.style.fontSize = "30px"
+}
